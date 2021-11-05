870865ee49d454817fc07d293ba0da01a3ed8930
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-11-05 17:13:07 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 2 - Teste o component NotFound', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  it('a página contém a mensagem de page request not found', () => {
+    const findTitlePage = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(findTitlePage).toBeInTheDocument();
+  });
+
+  it('a página contém uma imagem especifica', () => {
+    const findImg = screen.getAllByRole('img');
+
+    expect(findImg[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
