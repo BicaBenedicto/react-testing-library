2e189f747747fdb861829199927a3a3f2bd2eb87
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-11-05 16:46:24 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 2 - Teste o component about', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+
+  it('a página contém um titulo com About Pokedex', () => {
+    const findTitlePage = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(findTitlePage).toBeInTheDocument();
+  });
+
+  it('a página contem dois paragrados sobre a pokedex', () => {
+    const findParagraph = screen.getAllByText(/pokémons/i);
+
+    expect(findParagraph).toHaveLength(2);
+  });
+
+  it('a página contém a seguinte imagem de uma Pokédex', () => {
+    const findImg = screen.getByRole('img');
+
+    expect(findImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
